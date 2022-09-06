593096d391f41fb8d65d7a5ce375bf8db20f2427
Christopher Idowu <tommydprogrammer@gmail.com>
2022-09-06 05:45:48 +0100

    login page navigation addeD

--- handyman/View/Splash/SplashView.swift
+++ handyman/View/Splash/SplashView.swift
@@ -12,51 +12,53 @@ struct SplashView: View {
     
     
     var body: some View {
-        VStack {
-            
-            TabView(selection: $currentStep) {
+        NavigationView {
+            VStack {
                 
-                ForEach(0..<Constants().onboardings.count) { it in
-                    OnboardView(data: Constants().onboardings[it])
-                }
-            }
-            .tabViewStyle(.page(indexDisplayMode: .never))
-            
-            
-
-            
-            HStack{
-                HStack {
+                TabView(selection: $currentStep) {
+                    
                     ForEach(0..<Constants().onboardings.count) { it in
-                        
-                        if(it == self.currentStep) {
-                           Rectangle()
-                                .frame(width: 30, height: 10)
-                                .cornerRadius(10)
-                                .foregroundColor(.purple)
-                        }
-                        else {
-                            Circle()
-                                .frame(width: 10, height: 10)
-                                .foregroundColor(.gray)
-                        }
+                        OnboardView(data: Constants().onboardings[it])
                     }
                 }
-                Spacer()
-                Button {
-                    print("Next")
-                } label: {
-                    Text(self.currentStep < Constants().onboardings.count - 1 ? "Next" : "Get Started")
-                        .foregroundColor(.purple)
-                        .font(.headline)
-                }
+                .tabViewStyle(.page(indexDisplayMode: .never))
+                
+                
 
                 
-            }
-        } .padding(.horizontal, 20)
-            .padding(.vertical, 20)
-        
-    }
+                HStack{
+                    HStack {
+                        ForEach(0..<Constants().onboardings.count) { it in
+                            
+                            if(it == self.currentStep) {
+                               Rectangle()
+                                    .frame(width: 30, height: 8)
+                                    .cornerRadius(10)
+                                    .foregroundColor(.purple)
+                            }
+                            else {
+                                Circle()
+                                    .frame(width: 10, height: 10)
+                                    .foregroundColor(.gray)
+                            }
+                        }
+                    }
+                    Spacer()
+                    NavigationLink {
+                        LoginView()
+                    } label: {
+                        Text(self.currentStep < Constants().onboardings.count - 1 ? "Next" : "Get Started")
+                            .foregroundColor(.purple)
+                            .font(.headline)
+                    }
+
+                    
+                }
+            } .padding(.horizontal, 20)
+                .padding(.vertical, 20)
+            
+        }
+        }
 }
 
 struct SplashView_Previews: PreviewProvider {
