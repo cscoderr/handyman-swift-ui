2869b00c10826a82ae87ef4aecbcdb38e8812ee9
Christopher Idowu <tommydprogrammer@gmail.com>
2022-09-06 02:41:11 +0100

    Initial Commit

---
+++ handyman/handymanApp.swift
@@ -0,0 +1,17 @@
+//
+//  handymanApp.swift
+//  handyman
+//
+//  Created by Tomiwa Idowu on 06/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct handymanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
